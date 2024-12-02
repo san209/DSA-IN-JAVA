bde2910cdca9f1e25a637f63429b6321a366e7f8
san209 <survin0902@gmail.com>
2024-12-02 13:37:12 +0000

    insertion sort

--- sorting2.java
+++ sorting2.java
@@ -10,7 +10,9 @@ class sorting2 {
     public static void main(String args[]) {
         int arr[] = {7,8,3,1,2};
 
-        for(int i=0; i<arr.length; i++) {
+        //selection sort
+        //time complexity O(n^2)
+        for(int i=0; i<arr.length-1; i++) {
             int smallest = i;
             for(int j=i+1; j<arr.length; j++) {
                 if(arr[smallest]>arr[j]) {
