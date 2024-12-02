4545b559f20fcb7979b1daefe0871c270435d092
san209 <survin0902@gmail.com>
2024-12-02 10:01:38 +0000

    selection sort

--- sorting.java
+++ sorting.java
@@ -9,7 +9,7 @@ class sorting {
     }
     public static void main(String args[]) {
         int arr[] = {7,8,3,1,2};
-
+        //time complexity O(n^2)
         //bubble sort
         for(int i=0;i<arr.length-1; i++) {
             for(int j=0; j<arr.length-i-1; j++) {
